41243426064ba8f3746965278d607845916d29d9
mattSam <roadfen@gmail.com>
2016-03-11 15:59:06 +0100

    update class

--- src/tp/model/CityManagerService.java
+++ src/tp/model/CityManagerService.java
@@ -10,7 +10,16 @@ import java.util.List;
 public interface CityManagerService {
     boolean addCity(City city);
     boolean removeCity(City city);
+    void removeAll();
+    void setCities(List<City> cities);
+    List<City> searchFor(String cityName) throws CityNotFound;
+    List<City> searchCitiesNear(Position position) throws  CityNotFound;
+    City searchExactPosition(Position position) throws  CityNotFound;
+
+
     List<City> getCities();
+
+
 
 
 }
--- src/tp/rest/MyClient.java
+++ src/tp/rest/MyClient.java
@@ -21,12 +21,31 @@ public class MyClient {
 		System.out.println(cityManager.getCities());
 		City c = new City("Zanarkand", 16,64, "Hyrule");
 		cityManager.addCity(c);
-
-		System.out.println("city à supprimer : "+c.hashCode());
-
 		cityManager.removeCity(c);
-
 		System.out.println(cityManager.getCities());
+		cityManager.addCity(new City("Rouen", 49.443889, 1.103333,"FR"));
+		cityManager.addCity(new City("Mogadiscio", 49.443889, 1.103333,"SO"));
+		cityManager.addCity(new City("Rouen", 49.443889, 1.103333,"FR"));
+		cityManager.addCity(new City("Bihorel", 49.455278, 1.116944,"FR"));
+		cityManager.addCity(new City("Londres", 51.504872, -0.07857,"AN"));
+		cityManager.addCity(new City("Paris", 48.856578, 2.351828,"FR"));
+		cityManager.addCity(new City("Paris", 43.2, -80.38333,"FR"));
+
+		cityManager.getCities();
+		cityManager.addCity(new City("Villers-Bocage", 49.083333, -0.65,"FR"));
+		cityManager.addCity(new City("Villers-Bocage", 50.021858, 2.326126,"FR"));
+
+		cityManager.getCities();
+		cityManager.removeCity(new City("Villers-Bocage", 50.021858, 2.326126,"FR"));
+
+		try{
+			System.out.println(cityManager.searchFor("Villers-Bocage"));
+		}catch (Exception e){
+			e.getMessage();
+		}
+
+		cityManager.getCities();
+
 
 
 	}
